5aad053e1a1a8ebb2f5d58377ad56eb3303ede7e
DragonStramp <54375110+DragonStramp@users.noreply.github.com>
2020-02-21 17:15:07 -0800

    Redo

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-setInterval(function() {
-  if (window.pageYOffset === 0) {
-    nav.style.backgroundColor = ("rgba(0, 0, 0, 0)");
-  } else {
-    nav.style.backgroundColor = ("var(--bg-color)");
-  }
-}, 10);
+// var ship = document.getElementById("nav-image");
+// function shipOn() {
+//   ship.src = ("images/ship/ship-flame.png");
+// }
+// function shipOff() {
+//   ship.src = ("images/ship/ship.png");
+// }
