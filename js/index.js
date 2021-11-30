2b9c945eb9ebbf9e9dd69a1773e3a7b69494d7c1
DragonStramp <54375110+DragonStramp@users.noreply.github.com>
2021-11-30 03:52:25 -0700

    back to normal

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-var wave = wavify( document.querySelector('#wave'), {
-    height: 60,
-    bones: 3,
-    amplitude: 40,
-    color: 'rgb(255, 95, 126, 1)',
-    speed: .25
-  })
+var cnc = document.getElementById("contact");
+
+function closeContact() {
+    cnc.style.display = "none";
+}
+function bringUpContact() {
+    cnc.style.display = "block";
+}
