3f86f51ca09e672416f8baf582f60a8cec0f5e83
DragonStramp <54375110+DragonStramp@users.noreply.github.com>
2025-04-09 16:55:11 -0600

    update

--- ticket.js
+++ ticket.js
@@ -1,6 +1,7 @@
 var copyText = "";
 const followupCheck = document.getElementById("fucheck");
 const snipCheck = document.getElementById("snipcheck");
+const notescheck = document.getElementById("notecheck");
 const nameText = document.getElementById("nametext");
 const numberText = document.getElementById("numbertext");
 
@@ -26,17 +27,17 @@ function CopyAuthPin()
 {
   var emailText = "Hello, \n In recent communications with our support team, a copy of the authorization pin for your Mango Voice account was requested. \n \n Authorization Pin: " + document.getElementById("nametext").value + "\n \n This pin is necessary for any Mango Voice support agents to make changes to your account. \n Thank you, \n Mango Voice Support.";
   navigator.clipboard.writeText(emailText);
+  document.getElementById("pinbutton").text = "Copied!";
+  document.getElementById("pinbutton").classList.remove("button");
+  document.getElementById("pinbutton").classList.add("buttonpressed");
 }
 function CopyPasswordLink()
 {
   var emailText = "Hello, \n In recent communications with our support team, a password link was requested to access your Mango Voice account. After setting up your password with the link below, you'll be able to log in using the following information: \n \n Username: " + document.getElementById("username").value + "\n Password Link: " + document.getElementById("resetlink").value + "\n \n Thank you, \n Mango Voice Support.";
   navigator.clipboard.writeText(emailText);
-}
-function CopyProvLink()
-{
-}
-function CopyConfLink() 
-{
+  document.getElementById("passbutton").text = "Copied!";
+  document.getElementById("passbutton").classList.remove("button");
+  document.getElementById("passbutton").classList.add("buttonpressed");
 }
 
 function snipToggle()
@@ -79,8 +80,17 @@ const quill = new Quill('#editor', {
     },
   });
 
+  const noteseditor = new Quill('#notestext', {
+    modules: {
+    toolbar: false
+    },
+  });
+
 function SaveToClipboard() {
     document.getElementById("finishededitor").style.display = "block";
+    document.getElementById("copy").text = "Copied!";
+    document.getElementById("copy").classList.remove("button");
+    document.getElementById("copy").classList.add("buttonpressed");
     finishededitor.setContents([
         {insert: ""},
       ])
@@ -92,6 +102,12 @@ function SaveToClipboard() {
         finishededitor.insertText(0, 'Snippet:\n', 'bold', true);
       }
 
+      if(notescheck.checked == true)
+      {
+        finishededitor.insertText(0, noteseditor.getText() + "\n \n", 'bold', false);
+        finishededitor.insertText(0, 'Notes:\n', 'bold', true);
+      }      
+      
       finishededitor.insertText(0, quill2.getText() + "\n \n", 'bold', false);
       finishededitor.insertText(0, 'What I Did:\n', 'bold', true);
       finishededitor.insertText(0, quill.getText() + "\n \n", 'bold', false);
@@ -110,4 +126,6 @@ function SaveToClipboard() {
           finishededitor.insertText(0, fueditor.getText() + "\n", 'bold', false);
           finishededitor.insertText(0, 'Follow Up Reason:\n', 'bold', true);
         }
+    finishededitor.setSelection(0, finishededitor.getLength());
+    document.execCommand('copy');
 }
