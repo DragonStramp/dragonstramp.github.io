a47057683d18432d2711df12fd96976d1874e320
DragonStramp <daegon.stam@gmail.com>
2025-11-07 15:49:18 -0700

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -10,6 +10,8 @@ const eightAmPercent = document.getElementById("eightpercent");
 const nineAmTimer = document.getElementById("ninetimer");
 const nineAmPercent = document.getElementById("ninepercent");
 
+const rainbowGradient = 'linear-gradient(270deg, #ff0008ff, #ffe100ff, #00ff6aff, #00aeffff, #003cffff, #ff0000ff)';
+
 function getShiftWorkWeekProgress({startDay = 1, endDay = 5, shiftStart = 6} = {}) {
   const now = new Date();
   const currentDay = now.getDay();
@@ -54,28 +56,28 @@ function updateTimers() {
   const sixPercent = getShiftWorkWeekProgress({ shiftStart: 6 }).toFixed(2);
   sixAmTimer.style.width = sixPercent + "%";
   if(sixPercent >= 100) {
-    sixAmTimer.style.background = 'linear-gradient(270deg, #BA1C21, #d7a629ff, #1cba5eff, #1c88baff, #1c41baff)';
+    sixAmTimer.style.background = rainbowGradient
   }
   sixAmPercent.innerText = `6AM SHIFT: ${sixPercent}%`;
 
   const sevenPercent = getShiftWorkWeekProgress({ shiftStart: 7 }).toFixed(6);
   sevenAmTimer.style.width = sevenPercent + "%";
   if(sevenPercent >= 100) {
-    sevenAmTimer.style.background = 'linear-gradient(270deg, #BA1C21, #d7a629ff, #1cba5eff, #1c88baff, #1c41baff)';
+    sevenAmTimer.style.background = rainbowGradient;
   }
   sevenAmPercent.innerText = `7AM SHIFT: ${sevenPercent}%`;
 
   const eightPercent = getShiftWorkWeekProgress({ shiftStart: 8 }).toFixed(3);
   eightAmTimer.style.width = eightPercent + "%";
   if(eightPercent >= 100) {
-    eightAmTimer.style.background = 'linear-gradient(270deg, #BA1C21, #d7a629ff, #1cba5eff, #1c88baff, #1c41baff)';
+    eightAmTimer.style.background = rainbowGradient;
   }
   eightAmPercent.innerText = `8AM SHIFT: ${eightPercent}%`;
 
   const ninePercent = getShiftWorkWeekProgress({ shiftStart: 9 }).toFixed(3);
   nineAmTimer.style.width = ninePercent + "%";
   if(ninePercent >= 100) {
-    nineAmTimer.style.background = 'linear-gradient(270deg, #BA1C21, #d7a629ff, #1cba5eff, #1c88baff, #1c41baff)';
+    nineAmTimer.style.background = rainbowGradient;
   }
   nineAmPercent.innerText = `9AM SHIFT: ${ninePercent}%`;
 }
