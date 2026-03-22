f886c2718c3f02e0353507b8a722cdc26962d0ba
DragonStramp <54375110+DragonStramp@users.noreply.github.com>
2026-03-22 12:55:35 -0600

    CDR Step 1 Completed

--- js/cdr-parser.js
+++ js/cdr-parser.js
@@ -72,53 +72,100 @@ function pullInfo(selector, displayId, displayText) {
 }
 
 function updateUI() {
-
-    displayMajorErrors();   
-
     const mainFlow = Array.isArray(leg.callflow) ? leg.callflow[0] : leg.callflow;
+    let start = new Date(parseInt(mainFlow.times.created_time) / 1000).toLocaleString();
+    let end = new Date(parseInt(mainFlow.times.hangup_time) / 1000).toLocaleString();
 
-    if (mainFlow && mainFlow.times) {
-        let start = new Date(parseInt(mainFlow.times.created_time) / 1000).toLocaleString();
-        let end = new Date(parseInt(mainFlow.times.hangup_time) / 1000).toLocaleString();
-        pullInfo(start, "a-call-started", "Call Started: ");
-        pullInfo(end, "a-call-ended", "Call Ended: ");
-    } else {
-        pullInfo(null, "a-call-started", "Call Started: ");
-        pullInfo(null, "a-call-ended", "Call Ended: ");
-    }
+    pullInfo(start, "a-call-started", "Call Started: ");
+    pullInfo(end, "a-call-ended", "Call Ended: ");
 
-    if (leg['call-stats'] && leg['call-stats'].audio && leg['call-stats'].audio.inbound) {
-        let inbound = leg['call-stats'].audio.inbound;
-        pullInfo(inbound["media_packet_count"], "a-media-packet-count", "Media Packet Count: ");
-        pullInfo(inbound["skip_packet_count"], "a-skipped-packets", "Skipped Packet Count: ");
-        pullInfo(inbound["mos"], "a-mos", "MOS: ");
-    }
+    pullInfo(leg.variables["hangup_cause"], "a-hangup-cause", "Hangup Cause: ");
+    pullInfo(leg.variables["sip_hangup_disposition"], "a-sip-hangup-disposition", "SIP Hangup Disposition: ");
+    pullInfo(leg.variables["digits_dialed"], "a-digits-dialed", "Digits Dialed: ");
 
-    if (leg.variables) {
-        pullInfo(leg.variables["hangup_cause"], "a-hangup-cause", "Hangup Cause: ");
-        pullInfo(leg.variables["sip_hangup_disposition"], "a-sip-hangup-disposition", "SIP Hangup Disposition: ");
-        pullInfo(leg.variables["digits_dialed"], "a-digits-dialed", "Digits Dialed: ");
-    }
+    displayCallStats();
+    displayMajorErrors();
 }
 
 function displayMajorErrors() {
+    
     const errorLog = leg['call-stats'].audio['error-log'];
-    const listContainer = document.getElementById("error-list");
+    const listContainer = document.getElementById("a-error-list");
+    let startTime = leg.callflow.times["created_time"] / 1000;
     
     listContainer.innerHTML = '';
 
     if (errorLog && errorLog['error-period']) {
+        // pullInfo(errorLog['error-period'].length, "a-total-errors", "Total Flaws: ");
         const periods = Array.isArray(errorLog['error-period']) 
                         ? errorLog['error-period'] 
                         : [errorLog['error-period']];
 
-        periods.forEach(period => {
+        periods.toReversed().forEach(period => {
             const flawCount = parseInt(period.flaws);
             if (flawCount > 1) {
                 let li = document.createElement("li");
-                li.innerText = `Major Error: ${flawCount} flaws detected at start time ${period.start}`;
+                let errorTime = Math.floor(Math.abs(startTime - (period.start / 1000)) / 1000);
+                let errorMinute = Math.floor(errorTime / 60);
+                let errorSecond = errorTime % 60;
+                let errorTimeFormatted = `${errorMinute}:${errorSecond.toString().padStart(2, '0')}`;
+
+                li.innerText = `${flawCount} flaws at ${errorTimeFormatted}`;
+                if(flawCount > 5 && flawCount < 15)
+                {
+                    li.classList.add("text-warning");
+                } else if (flawCount >= 15) {
+                    li.classList.add("text-danger");
+                }
                 listContainer.appendChild(li);
             }
         });
+    } else {
+        document.getElementById("total-errors").innerText = "No Flaws Found!";
+    }
+}
+
+function displayCallStats()
+{
+    let inbound = leg['call-stats'].audio.inbound;
+
+    //Skipped packets
+    let mediaPackets = inbound["media_packet_count"];
+    let skippedPackets = inbound["skip_packet_count"];
+    let skippedPercent = ((Number(skippedPackets) / (Number(mediaPackets) + Number(skippedPackets))) * 100).toFixed(2);
+    pullInfo(mediaPackets, "a-media-packet-count", "Media Packet Count: ");
+    pullInfo(skippedPackets, "a-skipped-packets", "Skipped Packet Count: ");
+    pullInfo(skippedPercent + "%", "a-skipped-percent", "Percentage Skipped: ");
+
+    if(skippedPercent >= 15) {
+        document.getElementById("a-skipped-percent").classList.add("text-danger");
+    } else if (skippedPercent > 5 && skippedPercent < 15) {
+        document.getElementById("a-skipped-percent").classList.add("text-warning");
+    }
+    
+    //Quality Percentage
+    let qualityPercent = inbound["quality_percentage"];
+    
+    pullInfo(qualityPercent, "a-quality-percent", "Quality Percentage: ");
+
+    if(Number(qualityPercent) == 100) {
+        document.getElementById("a-quality-percent").classList.add("text-success");
+    } else if (Number(qualityPercent) < 100 && Number(qualityPercent) >= 80) {
+        document.getElementById("a-quality-percent").classList.add("text-warning");
+    } else {
+        document.getElementById("a-quality-percent").classList.add("text-danger");
+    }
+
+    //Mos
+
+    let mos = inbound["mos"];
+    pullInfo(mos, "a-mos", "MOS: ");
+
+    if(Number(mos) == 4.5) {
+        document.getElementById("a-mos").classList.add("text-success");
+    } else if (Number(mos) < 4.5 && Number(mos) >= 4) {
+        document.getElementById("a-mos").classList.add("text-warning");
+    } else {
+        document.getElementById("a-mos").classList.add("text-danger");
     }
 }
